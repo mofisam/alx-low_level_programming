292fbeedf4cd58b0a164fbeec23c5f6ce177bf66
Mofisam <malaosamuel2020@gmail.com>
2023-08-12 14:28:19 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num = '0';
+
+	for (num; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
