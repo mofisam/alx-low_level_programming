c697870683a51f53c6f7c8df246841bfb6117bd3
Mofisam <malaosamuel2020@gmail.com>
2023-08-12 14:50:27 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph <= 'a'; alph--)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
