a5108f72fd7bcb473d4651b61ff88748259fb8f1
Mofisam <malaosamuel2020@gmail.com>
2023-08-12 08:02:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descripption - prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
