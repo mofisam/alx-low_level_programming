e628d63bc16c35932b60726be4c4f7df39221520
Mofisam <malaosamuel2020@gmail.com>
2023-08-18 13:20:48 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - The country prints the numbers, from 0 to 9,
- * Return: none
+ * print_numbers - The funtion prints the numbers, from 0 to 9,
+ * Return: 0
  */
 void print_numbers(void)
 {
@@ -10,7 +10,7 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar(i +'0');
 	}
 
 	_putchar('\n');
