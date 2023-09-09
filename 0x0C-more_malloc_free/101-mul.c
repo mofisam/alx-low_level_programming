1ffa58ea7f56ec5facf9c4d961d81fc33adf7f04
Mofisam <malaosamuel2020@gmail.com>
2023-09-08 18:22:14 -0700

    program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,16 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+
+#define ERR_MSG "Error"
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
  * Return: 0 if a non-digit is found, 1 otherwise
  */
+
 int is_digit(char *s)
+
 {
+
 	int i = 0;
 
 	while (s[i])
+
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
@@ -19,12 +25,14 @@ int is_digit(char *s)
 	return (1);
 }
 
+
+
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
@@ -39,6 +47,7 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
+
 void errors(void)
 {
 	printf("Error\n");
@@ -51,6 +60,7 @@ void errors(void)
  * @argv: array of arguments
  * Return: always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
