0556527231edeb37ee42ba5fbc336bdd42fd3d29
Mofisam <malaosamuel2020@gmail.com>
2023-09-12 15:32:13 -0700

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 /**
  * main - program that prints the name of the file it was compiled from,
  * followed by a new line
- * Return: nothing
+ * Return: nothing to return back
  */
 int main(void)
 {
