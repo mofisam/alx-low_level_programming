fc2286eb5682a197f6290beb94088cb2eb22ea6e
Mofisam <malaosamuel2020@gmail.com>
2023-09-12 05:46:30 -0700

    How to use typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,13 @@
  * @age: the age of dog
  * @owner: the ower of the dog
  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
