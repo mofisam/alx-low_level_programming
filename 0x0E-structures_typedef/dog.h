00d714f42b3343480c3250483eeb0ad29a2c5b68
Mofisam <malaosamuel2020@gmail.com>
2023-09-11 18:40:05 -0700

    function that frees dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,11 @@
  * @age: the age of dog
  * @owner: the ower of the dog
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-typedef struct dog dog_t;
-my_dog *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
