11111f995f0bd4f2b4cff674c364d4f7c3f112f7
mofisam <malaosamuel2020@gmail.com>
2023-09-26 05:04:10 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * listint_len - function that returns the number of elements in a linked listint_t list
+ * @h: Pointer to a list.
+ * Return: Integer.
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned my_count;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		my_count++;
+		h = h->next;
+	}
+	return (my_count);
+}
