fb469fbaae6c7e62c121e0b773f7073612edef7f
mofisam <malaosamuel2020@gmail.com>
2023-09-25 17:18:37 -0700

    0x13. C - More singly linked lists

--- lists.h
+++ lists.h
@@ -2,6 +2,18 @@
 #define "HEADER_FILE"
 #include <stdio.h>
 
+/**
+ * struct listint_s - singly linked list function
+ * @n: integer
+ * @next: this points to the next node
+ * Description: the singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 
 #endif
