4aadc7dd87b843f20343ce3f3f5d545193d0fe87
mofisam <malaosamuel2020@gmail.com>
2023-09-26 18:49:50 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * add_nodeint_end - function that adds a new node at the beginning of a listint_t list.
+ * add_nodeint_end - function that adds a new node at the beginning of a
+ * listint_t list.
  * @head: This is my struct
  * @n: This is the entry point
  * Return: the address of the new element, or NULL if it failed.
