e955ffd0586f53076d631faec82c18a0fea2b476
mofisam <malaosamuel2020@gmail.com>
2023-09-26 18:29:55 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * add_nodeint_end - function that adds a new node at the beginning of a listint_t list.
  * @head: This is my struct
@@ -8,15 +7,24 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *n_node = (listint_t *) malloc(sizeof(listint_t));
+	listint_t *last = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
+	if (n_node == NULL)
+	{
+		free(n_node);
+		return (n_node);
+	}
+	n_node->n = n;
+	n_node->next = NULL;
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
+	if (*head == NULL)
+	{
+		*head = n_node;
+		return (*head);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = n_node;
+	return (*head);
 }
