e6060f90043d7987d2f6acf6fc4a7fbbf2324364
mofisam <malaosamuel2020@gmail.com>
2023-09-28 18:16:04 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,14 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-#include<string.h>
+
 #include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
