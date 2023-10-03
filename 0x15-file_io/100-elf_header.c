c4097c18b4ad34aaa770aeba9ecffb9e7e9be5ae
mofisam <malaosamuel2020@gmail.com>
2023-10-02 20:18:59 -0700

    0x15. C - File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -21,48 +21,48 @@ void close_elf(int elf);
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        dprintf(STDERR_FILENO, "Usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s <filename>\n", argv[0]);
+		return (1);
+	}
 
-    int o = open(argv[1], O_RDONLY);
-    if (o == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-        return 1;
-    }
+	int o = open(argv[1], O_RDONLY);
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		return (1);
+	}
 
-    Elf64_Ehdr *header = malloc(sizeof(Elf64_Ehdr));
-    if (header == NULL)
-    {
-        close_elf(o);
-        dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
-        return 1;
-    }
+	Elf64_Ehdr *header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
+		return (1);
+	}
 
-    ssize_t r = read(o, header, sizeof(Elf64_Ehdr));
-    if (r == -1)
-    {
-        free(header);
-        close_elf(o);
-        dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-        return 1;
-    }
+	ssize_t r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		return (1);
+	}
 
-    check_elf(header->e_ident);
-    printf("ELF Header:\n");
-    print_magic(header->e_ident);
-    print_class(header->e_ident);
-    print_data(header->e_ident);
-    print_version(header->e_ident);
-    print_osabi(header->e_ident);
-    print_abi(header->e_ident);
-    print_type(header->e_type, header->e_ident);
-    print_entry(header->e_entry, header->e_ident);
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
-    free(header);
-    close_elf(o);
-    return 0;
+	free(header);
+	close_elf(o);
+	return (0);
 }
