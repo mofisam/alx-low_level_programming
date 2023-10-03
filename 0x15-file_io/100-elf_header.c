7c4fc9d134a520c8d210a015a94495a15cabe49b
mofisam <malaosamuel2020@gmail.com>
2023-10-02 19:54:35 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+/**
+ * main - Displays the information contained in the ELF header
+ *        at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        dprintf(STDERR_FILENO, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    int o = open(argv[1], O_RDONLY);
+    if (o == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+        return 1;
+    }
+
+    Elf64_Ehdr *header = malloc(sizeof(Elf64_Ehdr));
+    if (header == NULL)
+    {
+        close_elf(o);
+        dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
+        return 1;
+    }
+
+    ssize_t r = read(o, header, sizeof(Elf64_Ehdr));
+    if (r == -1)
+    {
+        free(header);
+        close_elf(o);
+        dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+        return 1;
+    }
+
+    check_elf(header->e_ident);
+    printf("ELF Header:\n");
+    print_magic(header->e_ident);
+    print_class(header->e_ident);
+    print_data(header->e_ident);
+    print_version(header->e_ident);
+    print_osabi(header->e_ident);
+    print_abi(header->e_ident);
+    print_type(header->e_type, header->e_ident);
+    print_entry(header->e_entry, header->e_ident);
+
+    free(header);
+    close_elf(o);
+    return 0;
+}
