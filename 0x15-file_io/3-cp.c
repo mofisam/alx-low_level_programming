3f082488abda8a4ff518565310bf6ca26a13fd60
mofisam <malaosamuel2020@gmail.com>
2023-10-02 19:36:45 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error_file - Checks if files can be opened.
+ * @file_from: File descriptor for the source file.
+ * @file_to: File descriptor for the destination file.
+ * @argv: Arguments vector.
+ *
+ * Return: No return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - Main function for copying files.
+ * @argc: Number of arguments.
+ * @argv: Arguments vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	do
+	{
+		nchars = read(file_from, buf, BUFFER_SIZE);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	} while (nchars == BUFFER_SIZE);
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
